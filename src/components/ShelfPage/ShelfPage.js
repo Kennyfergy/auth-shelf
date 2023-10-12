21586f570cbd3cb1daa34c1b3158424362d022df
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-10-12 15:14:19 -0500

    added mui and some css

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,7 +1,21 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Button,
+  Container,
+  TextField,
+  Typography,
+  List,
+  ListItem,
+  IconButton,
+  ImageList,
+  ImageListItem,
+  Slide,
+  Tooltip,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import "./ShelfPage.css";
 
 function ShelfPage() {
   const [description, setDescription] = useState("");
@@ -11,60 +25,101 @@ function ShelfPage() {
 
   useEffect(() => {
     dispatch({ type: "FETCH_SHELF" });
-  }, []);
+  }, [dispatch]);
 
   const handleAddItem = (event) => {
     event.preventDefault();
     const newItem = { description, imageUrl };
     dispatch({ type: "ADD_ITEM", payload: newItem });
+    setDescription("");
+    setImageUrl("");
   };
 
-  // console.log(store);
   return (
-    <div className="container">
-      <h2>Shelf</h2>
-      <div>
-        <p>Add new item</p>
-        <form onSubmit={handleAddItem}>
-          <input
-            type="text"
-            name="description"
-            value={description}
-            placeholder="Description"
-            required
-            onChange={(event) => setDescription(event.target.value)}
-          />
-          <input
-            value={imageUrl}
-            name="imageUrl"
-            placeholder="Full Image URL"
-            required
-            onChange={(event) => setImageUrl(event.target.value)}
-          />
-          <button>SUBMIT</button>
-        </form>
-      </div>
-      <p>All of the available items can be seen here.</p>
-      <div>
-        {store.map((item) => (
-          <ul key={item.id}>
-            <li>
-              {item.description}
-
-              <img src={item.image_url} />
-              <button
+    <Container>
+      <Typography variant="h2" gutterBottom>
+        Shelf
+      </Typography>
+      <Typography paragraph>Add image</Typography>
+      <form onSubmit={handleAddItem}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          id="description"
+          label="Description"
+          name="description"
+          autoFocus
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+          size="small"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          name="imageUrl"
+          label="Full Image URL"
+          id="imageUrl"
+          value={imageUrl}
+          onChange={(event) => setImageUrl(event.target.value)}
+          size="small"
+        />
+        <br />
+        <Tooltip title="Add Item" arrow>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="medium"
+            startIcon={<AddCircleOutlineIcon />}
+            sx={{ mt: 1 }}
+          >
+            Submit
+          </Button>
+        </Tooltip>
+      </form>
+      <Typography paragraph></Typography>
+      <List>
+        {store.map((item, index) => (
+          <Slide
+            direction="up"
+            in={true}
+            mountOnEnter
+            unmountOnExit
+            key={item.id}
+          >
+            <ListItem>
+              <ImageList variant="masonry" cols={1} gap={8}>
+                <ImageListItem>
+                  <img
+                    src={item.image_url}
+                    alt={item.description}
+                    loading="lazy"
+                    style={{
+                      maxHeight: "200px",
+                      maxWidth: "auto",
+                      objectFit: "cover",
+                    }}
+                  />
+                </ImageListItem>
+              </ImageList>
+              <Typography>{item.description}</Typography>
+              <IconButton
+                edge="end"
+                aria-label="delete"
                 onClick={() =>
                   dispatch({ type: "DELETE_ITEM", payload: item.id })
                 }
+                size="small"
               >
-                {" "}
-                Delete{" "}
-              </button>
-            </li>
-          </ul>
+                <DeleteIcon />
+              </IconButton>
+            </ListItem>
+          </Slide>
         ))}
-      </div>
-    </div>
+      </List>
+    </Container>
   );
 }
 
